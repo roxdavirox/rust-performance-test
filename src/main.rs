76d772a3e1bfdb4385bed34356d93979095424cd
roxdavirox <roxdavirox@gmail.com>
2025-05-09 20:50:16 -0300

    feat: add server restful

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-  println!("Hello, world");
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+async fn hello_world() -> &'static str {
+    "Hello, world from Rust in Docker!"
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("🚀 Listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
