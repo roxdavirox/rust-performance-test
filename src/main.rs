fdccc2aae25094e3fb4738a632a87154df417e61
roxdavirox <roxdavirox@gmail.com>
2025-05-09 21:19:59 -0300

    fix: build

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use axum::{routing::get, Router};
+use axum::{routing::get, Router, Server};
 use std::net::SocketAddr;
-use hyper::Server;
 
 #[tokio::main]
 async fn main() {
-  let app = Router::new().route("/", get(|| async { "Hello, world from Axum + Hyper!" }));
-  let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-  println!("🚀 Listening on {}", addr);
+    let app = Router::new().route("/", get(|| async { "Hello from Axum!" }));
 
-  Server::bind(&addr)
-    .serve(app.into_make_service())
-    .await
-    .unwrap();
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+    println!("🚀 Listening on http://{}", addr);
+
+    Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
