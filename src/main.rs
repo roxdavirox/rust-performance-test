82db3cef40ffa5750eeacd8d61b543554c850d15
roxdavirox <roxdavirox@gmail.com>
2025-05-12 01:49:46 -0300

    WIP: configurando infra 2/2

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,14 @@
 use axum::{routing::post, Router};
 use sqlx::postgres::PgPoolOptions;
 use std::{net::SocketAddr, time::Duration};
-use tower_http::trace::{TraceLayer, DefaultMakeSpan, DefaultOnResponse};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 mod db;
 mod models;
 mod handlers;
 
 fn main() {
-	tracing_subscriber::registry()
-		.with(
-			EnvFilter::from_default_env()
-				.add_directive("axum=info".parse().unwrap())
-				.add_directive("tower_http=info".parse().unwrap()), // opcional: detalha middleware
-		)
-		.with(tracing_subscriber::fmt::layer())
-		.init();
-
 	tokio::runtime::Builder::new_multi_thread()
-		.worker_threads(num_cpus::get())
+		.worker_threads(2)
 		.enable_all()
 		.build()
 		.unwrap()
@@ -28,24 +17,18 @@ fn main() {
 
 async fn async_main() {
 	let pool = PgPoolOptions::new()
-		.max_connections(200)
+		.max_connections(4)
 		.min_connections(0)
-		.acquire_timeout(Duration::from_secs(10))
 		.connect("postgres://postgres:password@db:5432/rinha")
 		.await
 		.expect("❌ Falha ao conectar no banco de dados");
 
-	let app = Router::new()
-		.route("/pessoas", post(handlers::post_pessoa))
-		.with_state(pool)
-		.layer(
-			TraceLayer::new_for_http()
-				.make_span_with(DefaultMakeSpan::new().include_headers(true))
-				.on_response(DefaultOnResponse::new().include_headers(true)),
-		);
+    let app = Router::new()
+        .route("/pessoas", post(handlers::post_pessoa))
+        .with_state(pool);
 
-	let addr = SocketAddr::from(([0, 0, 0, 0], 80));
-	tracing::info!("🚀 Servidor rodando em http://{}", addr);
+    let addr = SocketAddr::from(([0, 0, 0, 0], 80));
+    println!("🚀 Rodando em http://{}", addr);
 
 	axum::Server::bind(&addr)
 		.serve(app.into_make_service())
